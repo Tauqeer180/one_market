0b718a5bc31dbe27279fd311cecef84030968213
TauqeerCybex <tauqeermalik855@gmail.com>
2023-07-22 09:08:36 +0500

    init

--- axios/index.js
+++ axios/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const ip = '192.168.1.4';
+const ip = "192.168.1.4";
 const axiosBase = axios.create({
-    baseURL: `http://${ip}:3001`
-})
+  baseURL: `https://node.animalspetinfo.com`,
+});
 
 export default axiosBase;
